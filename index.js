4ddc84e7b5d9c659da1204fb32057cf901e29781
Mat Taylor <mtaylor@mtaylor-mbp15.corp.roku>
2017-09-01 10:55:20 -0700

    better directory handling

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
 var brs = require('./brightscript.js')
 var cmd = require('commander')
 var fs  = require('fs')
+var mkdir = require('mkdirp')
 
 
 cmd
-.version('0.1.0')
-.usage('[options] <file ...>')
-.option('-p, --parse', 'Parse only')
-.option('-i, --dir <inputDir>')
-.option('-o, --out <outputDir>', 'Name of the output file')
-//.command('parse [file]', 'Parse JS file')
-//.command('build [file]', 'Convert Web Component to Brightscript')
-//.comman('trans [file]', 'Convert single JS file to BrightScript', {isDefault: true})
-.parse(process.argv)
+	.version('0.1.0')
+	.usage('[options] <file ...>')
+	.option('-p, --parse', 'Parse only')
+	.option('-i, --inpDir <inpDir>')
+	.option('-o, --outDir <outDir>', 'Name of the output Directory')
+	.parse(process.argv)
 
+cmd.args.map(compile)
 
-cmd.args.map(file => {
-	var jsPath = (cmd.inpDir||'./')+file
-	console.log('Parsing ', file)
-	try { 
-		var bsPath = (cmd.inpDir||'./')+file.replace(/.js$/, '.brs') 
+ 
+function compile(path) {
+	var jsPath = (cmd.inpDir||'.')+'/'+path
+	var bsPath = (cmd.outDir||'.')+'/'+path.replace(/.js$/, '.brs') 
+	if (fs.statSync(jsPath).isDirectory()) {
+		if (!fs.existsSync(bsPath)) mkdir.sync(bsPath)
+		return fs.readdirSync(jsPath).forEach(file => compile(path+'/'+file))
+	}
+	if (path.includes('invalid') || !path.endsWith('.js')) return
+	console.log(`\nCompiling ${jsPath} to ${bsPath}`)
+	try {
 		var jsCode = fs.readFileSync(jsPath, 'utf-8')
 		var bsCode = brs.build(jsCode)
 		fs.writeFileSync(bsPath, bsCode, 'utf-8')
 	} catch(e) { 
 		console.log(e.message)
 	}
-})
+}
